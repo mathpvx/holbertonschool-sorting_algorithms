804c96bfc0745e937f9609a62fd494950e449212
mathpvx <mathilde.pavaux@gmail.com>
2024-01-15 07:30:16 -0800

    header file

---
+++ sort.h
@@ -0,0 +1,19 @@
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/**print functions prototypes*/
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/** other functions prototypes*/
+void bubble_sort(int *array, size_t size);

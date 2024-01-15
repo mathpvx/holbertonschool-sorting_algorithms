99087f458c815ebf97a18b1621115492e0626224
mathpvx <mathilde.pavaux@gmail.com>
2024-01-15 07:30:46 -0800

    bubble sorting task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts the array with bubble sorting
+ * @array: array of numbers to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i;
+
+	for (i = 0; i < size - 2; i++)
+	{
+		if (array[i] > array[i + 1])
+			/**swap*/
+			/**print*/
+	}
+
+	if in order
+		return;
+}

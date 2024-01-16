179bf504d9c52b2768c0bf84550fde10628173c4
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-16 11:42:08 +0100

    Added function prototype

--- sort.h
+++ sort.h
@@ -21,4 +21,5 @@ void print_array(const int *array, size_t size);
 
 /** other functions prototypes*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 #endif
